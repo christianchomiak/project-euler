4b1793b95b1923b6aea481c0e1ed34b572a5dec2
Christian Chomiak <christianchomiak@gmail.com>
2020-01-08 19:52:31 +0100

    Updated solution 1 in Python
    It now accepts a command line argument passing the input (it defaults to the one from Project Euler) and also an option to run unit tests

--- src/Python/problem_001.py
+++ src/Python/problem_001.py
@@ -5,9 +5,16 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def test_solution():
-    assert sum_all_multiples(10) == 23
-    assert sum_all_multiples(1000) == 233168
+import sys
+
+def test_solution(input, expected_result):
+    result = sum_all_multiples(input)
+    assert result == expected_result, "Test FAILED, expected: " + str(expected_result) + ", got: " + str(result)
+
+def test_solutions():
+    test_solution(10, 23)
+    test_solution(1000, 233168)
+    print("Tests OK")
 
 def sum_all_multiples(limit):
     sum = 0
@@ -17,10 +24,23 @@ def sum_all_multiples(limit):
     return sum
 
 def main():
-    limit = 1000
+    if len(sys.argv) - 1 == 0:
+        # No argument? Default it!
+        limit = 1000
+    else:
+        # Even if there are more than one
+        # arguments, we only care about the
+        # first one, so let's continue.
+
+        if sys.argv[1].isnumeric():
+            limit = int(sys.argv[1])
+        elif sys.argv[1] == 'test':
+            test_solutions()
+            return
+        else:
+            sys.exit("Expected an integer")
     result = sum_all_multiples(limit)
     print(result)
 
 if __name__ == '__main__':
     main()
-    test_solution()
